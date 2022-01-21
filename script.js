db2c631d38bd71c6f59c810e8b2f427d264c59a8
phamha98 <phamtranquangha@gmail.com>
2022-01-21 16:49:49 +0700

    clear loading

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 #!/usr/bin/env node
 
 console.log('This is post init script')
-console.log('Đang cài chờ chút nhé @@ ahihi !!')
-
-const P = ['\\', '|', '/', '-']
-let x = 0
-const loader = setInterval(() => {
-  process.stdout.write(`\r${P[x++]}`)
-  x %= P.length
-}, 250)
-
-setTimeout(() => {
-  clearInterval(loader)
-}, 3000)
+console.log('Loading...')
