61adb5709cee15c353e11078ad1dac20fbc4876c
phamha98 <phamtranquangha@gmail.com>
2022-01-21 14:23:04 +0700

    placeholderName

--- template.config.js
+++ template.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: 'rnapp',
+  placeholderName: 'ProjectName',
 
   // Directory with template
   templateDir: './template',
