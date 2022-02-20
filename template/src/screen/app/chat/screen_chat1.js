91393d42e26fd9e030e48400ae8d9e3e5855a290
phamha98 <phamtranquangha@gmail.com>
2022-02-21 06:48:03 +0700

    ff

--- template/src/screen/app/chat/screen_chat1.js
+++ template/src/screen/app/chat/screen_chat1.js
@@ -1,42 +1,99 @@
-import {StyleSheet, Text, View, Image} from 'react-native'
-import React, {useEffect} from 'react'
-import {IMAGE} from '@assets/image'
-import {navigate, replace} from '@navigation'
-
-export default function screen_chat1 () {
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  Dimensions,
+} from 'react-native'
+import React, {useEffect, useRef, useState} from 'react'
+import io from 'socket.io-client'
+import {InputCoreC, TextCoreC, ViewCoreC} from '@mylib'
+export const domain = 'http://192.168.0.102:3000/'
+export default function screen_chat2 () {
+  const textValue = useRef()
+  const [data, setData] = useState(['hello'])
+  const socket = io(domain)
   useEffect(() => {
-    let clear = setTimeout(() => {
-      replace('screen_chat2')
-    }, 500)
+    console.log('useEffect')
+    socket.on('connection', e => {
+      console.log('**Ket noi thanh cong', e)
+    })
+    socket.on('chat message', msg => {
+      setData(state => [...state, msg])
+    })
     return () => {
-      clearTimeout(clear)
+      socket.off('connection')
+      socket.off('chat message')
+      // socket.off('message');
     }
   }, [])
+  const send = () => {
+    let value = textValue.current.getValue()
+    console.log(value)
+    socket.emit('chat message', value)
+    textValue.current.clear()
+  }
   return (
-    <View style={styles.constainer}>
-      <Image
-        style={{width: 60, height: 60, resizeMode: 'contain', marginBottom: 40}}
-        source={IMAGE.messeger}
+    <View style={styles.a}>
+      <Text>Chat</Text>
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        data={data}
+        renderItem={({item}) => (
+          <ViewCoreC paddingLeft={10} width={Dimensions.get('screen').width}>
+            <ViewCoreC style={styles.itemChat}>
+              <TextCoreC bold style={{color: '#fff'}}>
+                {item}
+              </TextCoreC>
+            </ViewCoreC>
+          </ViewCoreC>
+        )}
+        contentContainerStyle={{flex: 1, width: Dimensions.get('screen').width}}
       />
-      <View style={{alignItems: 'center', position: 'absolute', bottom: 20}}>
-        <Text
-          style={{position: 'absolute', color: '#AFADAD', fontWeight: '300'}}>
-          from
-        </Text>
-        <Image
-          style={{width: 60, height: 60, resizeMode: 'contain'}}
-          source={IMAGE.meta_logo}
+      <ViewCoreC row style={styles.boxChat}>
+        <InputCoreC
+          ref={textValue}
+          style={styles.input}
+          placeholder='Nhập tại đây'
+          onSubmitEditing={send}
         />
-      </View>
+        <Button title={'Send'} onPress={send} />
+      </ViewCoreC>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  constainer: {
+  a: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'#fff'
+  },
+  input: {
+    height: 50,
+    width: '80%',
+    backgroundColor: 'pink',
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 18,
+  },
+  boxChat: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  itemChat: {
+    width: 80,
+    height: 40,
+    backgroundColor: 'darkturquoise',
+    borderRadius: 100,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff',
   },
 })
